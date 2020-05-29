85cb727ca10c0a9ea7a567d95a189d152ffa2aff
foo <foo@bar.de>
2020-05-30 00:44:49 +0200

    feat: add release config

--- src/index.js
+++ src/index.js
@@ -1,8 +1,12 @@
 import { outputFile } from 'fs-extra'
 import getPackageName from 'get-package-name'
+import loadPkg from 'load-pkg'
+import baseConfig from './base-config'
 import dev from './dev'
 import prepublishOnly from './prepublish-only'
 import lint from './lint'
+
+const packageConfig = loadPkg.sync() || {}
 
 export default {
   allowedMatches: [
@@ -42,4 +46,31 @@ export default {
     },
     prepublishOnly,
   },
+  deployPlugins: [
+    [
+      getPackageName(require.resolve('@semantic-release/exec')),
+      {
+        prepareCmd: 'yarn prepublishOnly',
+      },
+    ],
+    [
+      getPackageName(require.resolve('semantic-release-chrome')),
+      {
+        asset: 'extension.zip',
+        extensionId: baseConfig.chromeExtensionId,
+        ...(packageConfig.isPrivate && { target: 'trustedTesters' }),
+      },
+    ],
+  ],
+  deployEnv: {
+    GOOGLE_CLIENT_ID: '${{ secrets.GOOGLE_CLIENT_ID }}',
+    GOOGLE_CLIENT_SECRET: '${{ secrets.GOOGLE_CLIENT_SECRET }}',
+    GOOGLE_REFRESH_TOKEN: '${{ secrets.GOOGLE_REFRESH_TOKEN }}',
+  },
+  deployAssets: [
+    {
+      path: 'extension.zip',
+      label: 'Extension',
+    },
+  ],
 }
