60af61a6f6dd244ff7c57278c414a4a91155d64a
Sebastian Landwehr <info@sebastianlandwehr.com>
2024-01-25 00:02:40 +0100

    fix: revert change

--- src/dev.js
+++ src/dev.js
@@ -3,7 +3,7 @@ import webpack from 'webpack'
 import getWebpackConfig from './get-webpack-config.js'
 
 export default config =>
-  (target = 'chromium') => {
+  (target = 'firefox-desktop') => {
     process.env.WEB_EXT_TARGET = target
 
     return new Promise(() =>
